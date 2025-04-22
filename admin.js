14d920947a2e3463eaab1da49db825d32936693b
Jorge Luis <l24171084@culiacan.tecnm.mx>
2025-04-22 07:07:08 +0000

    si

--- admin.js
+++ admin.js
@@ -11,26 +11,7 @@
       localStorage.setItem('users', JSON.stringify(users));
     }
 
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-      const username = document.getElementById('newUsername').value.trim();
-      const password = document.getElementById('newPassword').value.trim();
-
-      const users = getUsers();
-
-      if (users.find(u => u.username === username)) {
-        msg.textContent = "Ese usuario ya existe.";
-        msg.style.color = "red";
-        return;
-      }
-
-      users.push({ username, password, role: "user" });
-      saveUsers(users);
-
-      msg.textContent = "Usuario creado exitosamente.";
-      msg.style.color = "green";
-      form.reset();
-    });
+    
 
     function logout() {
       localStorage.clear();
